6d3b7e36f66e0146f3fd015324565c795d4e3d36
Yu19980809 <1572250276@qq.com>
2023-04-23 22:29:31 +0800

    modified file structure

---
+++ src/constants/index.js
@@ -0,0 +1,22 @@
+export const navbarLinks = [
+    {
+        id: 'home',
+        title: 'Home'
+    },
+    {
+        id: 'about',
+        title: 'About'
+    },
+    {
+        id: 'service',
+        title: 'Service'
+    },
+    {
+        id: 'work',
+        title: 'Work'
+    },
+    {
+        id: 'contact',
+        title: 'Contact Us'
+    }
+]
