b35a38a9a7066f4cb0d658ccaca8307ac784aa8d
Yu19980809 <1572250276@qq.com>
2023-04-23 22:10:29 +0800

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,19 @@
+import NavBar from './NavBar';
+import Logo from './Logo';
+import About from './About';
+import Service from './Service';
+import Work from './Work';
+import Partner from './Partner';
+import Contact from './Contact';
+import Footer from './Footer';
+
+export {
+    NavBar,
+    Logo,
+    About,
+    Service,
+    Work,
+    Partner,
+    Contact,
+    Footer
+}
