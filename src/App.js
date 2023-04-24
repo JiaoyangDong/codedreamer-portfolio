8ec65083e5ce596f1c6285a7421ebdc53287e6a3
Yu19980809 <1572250276@qq.com>
2023-04-24 11:20:53 +0800

    installed aos

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 import { NavBar, Logo, About, Service, Work, Partner, Contact, Footer } from './components/index';
 
 const App = () => {
+  useEffect( () => {
+    AOS.init( { duration: 2000 } )
+  }, [] )
+
   return (
     <div>
       <NavBar />
